d621cb12d2301743cb290d8ac84816914afccb1d
tvinh321 <tvinh321@gmail.com>
2022-11-11 15:24:41 +0700

    feat(landing-header-footer): landing, header, footer, favicon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,44 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import {connect} from 'react-redux';
+//Import Icons
+import CartIcon from '../../assets/cart-icon.svg';
+import SearchIcon from '../../assets/search-icon.svg';
 
-import {createStructuredSelector} from 'reselect'
-import {selectCurrentUser} from '../../redux/user/user-selectors';
-import {selectCartHidden} from '../../redux/cart/cart-selectors';
+const Header = ({ HiHiClothesLogo }) => {
+    return (
+        <div className="absolute top-0 inset-x-0">
+            <div className="relative">
+                <div className="absolute flex top-8 right-16">
+                    <div className="grid grid-cols-3 gap-16 mr-24 text-xl font-mono text-hihiclothes-1">
+                        <h1 className="cursor-pointer">Home</h1>
+                        <h1 className="cursor-pointer">Shop</h1>
+                        <h1 className="cursor-pointer">Blogs</h1>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8">
+                        <img src={SearchIcon} alt="Search Icon" className="w-6 h-6 cursor-pointer object-contain" />
+                        <img src={CartIcon} alt="Cart Icon" className="w-6 h-6 cursor-pointer object-contain" />
+                    </div>
+                </div>
 
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+                <HiHiClothesLogo className='w-64 h-32 pl-16 pt-8 fill-hihiclothes-1 cursor-pointer' />
+            </div>
+        </div>
+    );
+}
 
-import {auth} from '../../firebase/firebase.utils';
-
-import CartIcon from '../cart-icon/CartIcon';
-import CartDropdown from '../cart-dropdown/CartDropdown';
-
-import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header-styles';
-
-const Header = ({ currentUser, hidden }) => (
-    <HeaderContainer>
-        <LogoContainer to='/'>
-            <Logo className='logo' />
-        </LogoContainer>
-        <OptionsContainer>
-            <OptionLink to='/shop'>SHOP</OptionLink>
-            <OptionLink to='/contact'>CONTACT</OptionLink>
-            {
-                currentUser ? // If user is authenticated
-                    <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
-                :
-                    <OptionLink to='/register'>REGISTER</OptionLink>
-            }
-            <CartIcon />
-        </OptionsContainer>
-        {hidden ? null : <CartDropdown />}
-    </HeaderContainer>
-);
-
-const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser,
-    hidden: selectCartHidden
-});
-
-
-export default connect(mapStateToProps)(Header);
+export default Header;
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,20 +1,116 @@
-import React, { Profiler } from 'react';
+import React from 'react';
 
-import Directory from '../../components/directory/Directory';
+// Import HiHiClothes Logo
+import { ReactComponent as HiHiClothesLogo } from '../../assets/hihiclothes-logo.svg';
 
-import {HomePageContainer} from './homepage-styles';
+// Import Images
+import WinterCollection from '../../assets/winter-collection.png';
+import AutumnCollection from '../../assets/autumn-collection.png';
+import NikeLogo from '../../assets/nike-logo.svg';
+import AdidasLogo from '../../assets/adidas-logo.png';
+import GucciLogo from '../../assets/gucci-logo.svg';
+import LouisVuittonLogo from '../../assets/louis-vuitton-logo.png';
+import ChanelLogo from '../../assets/chanel-logo.png';
+import Hats from '../../assets/hats.png';
+import Bags from '../../assets/bags.png';
+import Sneakers from '../../assets/sneakers.png';
+import Skirt from '../../assets/skirts.png';
+import Glasses from '../../assets/glasses.png';
+import Jeans from '../../assets/jeans.png';
+
+// Import Header and Footer
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
 const Homepage = () => (
-    <HomePageContainer>
-        <Profiler
-            id='directory'
-            onRender={(id, phase, actualDuration) => {
-                console.log({id, phase, actualDuration});
-            }}
-        >
-            <Directory />
-        </Profiler>
-    </HomePageContainer>
+    <div className="relative">
+        <Header HiHiClothesLogo={HiHiClothesLogo} />
+
+        <div style={{ backgroundColor: '#F4EBE6' }} className="w-full bg-hihiclothes-2 mt-0 flex max-h-[32rem]">
+            <div className="w-5/12 px-8 pt-40">
+                <div className="mt-28 ml-24 leading-loose text-hihiclothes-1 cursor-pointer">
+                    <h2 className="font-serif text-2xl">New Women's Collection</h2>
+                    <h1 className="text-6xl font-bold mt-3">Winter 2022</h1>
+                </div>
+            </div>
+            <div className="w-7/12">
+                <img src={WinterCollection} alt="Winter Collection" className="object-cover w-full h-full" />
+            </div>
+        </div>
+        
+        {/* Brands */}
+        <div className="w-full px-4 py-16">
+            <h1 className='text-center text-2xl text-hihiclothes-3 font-bold mb-8'>Brands</h1>
+            <div className='grid grid-cols-5 gap-20 place-items-center px-48'>
+                <img src={NikeLogo} alt="Nike Logo" className="object-contain w-32 h-32" />
+                <img src={AdidasLogo} alt="Adidas Logo" className="object-contain w-32 h-32" />
+                <img src={GucciLogo} alt="Gucci Logo" className="object-contain w-32 h-24" />
+                <img src={LouisVuittonLogo} alt="Louis Vuitton Logo" className="object-contain w-32 h-24" />
+                <img src={ChanelLogo} alt="Chanel Logo" className="object-contain w-32 h-24" />
+            </div>
+        </div>
+
+        {/* Summer Collection Sale  */}
+        <div className="w-full bg-hihiclothes-2 mt-0 flex cursor-pointer">
+            <div className='relative w-1/2'>
+                <div className="text-hihiclothes-3 absolute inset-x-0 bottom-0 text-right py-8 px-8">
+                    <h2 className='text-3xl mb-2'>20% Off</h2>
+                    <h1 className='text-5xl font-semibold'>SUMMER 2022 COLLECTION</h1>
+                </div>
+            </div>
+            <div className="w-1/2">
+                <img src={AutumnCollection} alt="Autumn Collection" className="object-cover w-full h-full" />
+            </div>
+        </div>
+
+        {/* Categories */}
+        <div className="w-full px-16 py-16 grid grid-cols-4 gap-12 tracking-widest">
+            <div className="w-full col-span-2 flex bg-[#DAE5E3] cursor-pointer">
+                <div className="w-1/2 flex flex-col justify-center px-16 py-4 text-[#405563]">
+                    <h2 className='font-light'>MOST WANTED</h2>
+                    <h2 className=' font-bold text-3xl'>Blue Jeans</h2>
+                </div>
+                <div className="w-1/2">
+                    <img src={Jeans} alt="Jeans" />
+                </div>
+            </div>
+            <div style={{ backgroundImage: `url(${Sneakers})` }} className='col-span-2 row-span-2 bg-cover relative cursor-pointer'>
+                <div className="absolute inset-x-0 bottom-0 py-16 flex flex-col items-center">
+                    <h2 className='text-hihiclothes-3 font-bold text-3xl'>Sneakers</h2>
+                    <h2 className='text-hihiclothes-3 font-light text-lg'>NEW COLLECTION</h2>
+                </div>
+            </div>
+            <div className="w-full col-span-2 flex bg-[#FCEBE2] cursor-pointer">
+                <div className="w-1/2">
+                    <img src={Bags} alt="Bags" />
+                </div>
+                <div className="w-1/2 flex flex-col justify-center px-16 py-4 text-[#C08E82]">
+                    <h2 className='text-sm font-light'>BRAND NEW</h2>
+                    <h2 className=' font-bold text-3xl'>Bags</h2>
+                </div>
+            </div>
+            <div className='bg-[#F3F5F8] cursor-pointer'>
+                <div className="h-1/2 flex items-center justify-center text-2xl font-semibold px-12 text-center">
+                    <h1 className="leading-relaxed">Will+Bear Andy Cream Hat</h1>
+                </div>
+                <img src={Hats} alt="Hats" />
+            </div>
+            <div className='w-full col-span-2 flex bg-[#C1E4DE] cursor-pointer'>
+                <div className="w-1/2 flex flex-col justify-center px-16 py-4 text-[#867A75]">
+                    <h2 className='font-bold text-3xl mb-4'>Skirts</h2>
+                    <h2 className='text-sm font-light'>From just $100</h2>
+                </div>
+                <div className="w-1/2">
+                    <img src={Skirt} alt="Skirt" className='object-cover w-full h-full' />
+                </div>
+            </div>
+            <div style={{ backgroundImage: `url(${Glasses})` }} className="bg-cover bg-bottom cursor-pointer">
+                <h1 className='mt-10 text-center text-2xl text-[#847E7E] font-light'>GLASSES</h1>
+            </div>
+        </div>
+
+        <Footer HiHiClothesLogo={HiHiClothesLogo} />
+    </div>
 );
 
 export default Homepage;
