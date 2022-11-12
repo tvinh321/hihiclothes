7345c2a5b6912b9f657430ccb07aea0a04895ac1
DESKTOP-FJU84G0\Admin <ngochoang240601@gmail.com>
2022-11-12 22:14:18 +0700

    Update UI for product-list

--- src/pages/product-list/product-list.jsx
+++ src/pages/product-list/product-list.jsx
@@ -17,7 +17,7 @@ const ProductList = () => (
 
         <div className="relative">
             <div class="bg-white mt-32 grid grid-cols-5 grid-flow-col">
-                <div class="w-xl-80 h-72 shadow-md bg-white relative px-1 mt-36 ml-28">
+                <div class="w-xl-80 h-72 shadow-lg bg-white relative px-1 mt-36 ml-28">
                     <ul class="relative content-center py-6">
                         <li class="relative text-center my-4">
                             <a class="items-center text-sm py-4 px-6 h-12 font-medium overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-red-900 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Nike</a>
@@ -42,11 +42,11 @@ const ProductList = () => (
                 <div class="col-span-4 mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                     <h2 class="text-2xl font-bold tracking-tight text-red-900 text-center">Shoes</h2>
                     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -54,16 +54,21 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -71,16 +76,21 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -88,16 +98,21 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -105,18 +120,23 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
                     </div>
                     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -124,16 +144,21 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -141,16 +166,21 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -158,16 +188,21 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
-                        <div class="group relative">
+                        <div class="group relative shadow-xl">
                             <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 <img src={Sneakers} alt="Front of men&#039;s Basic Shoes in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
                             </div>
-                            <div class="mt-4 flex justify-between">
+                            <div class="flex justify-between p-4">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
                                         <a href="#">
@@ -175,9 +210,14 @@ const ProductList = () => (
                                             Basic Shoes
                                         </a>
                                     </h3>
-                                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                                    <p class="mt-1 text-sm text-gray-500">$35</p>
                                 </div>
-                                <p class="text-sm font-medium text-gray-900">$35</p>
+                                <div>
+                                    {/* <p class="text-sm font-medium text-red-900">$35</p> */}
+                                    <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-red-800 text-red-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Detail</button>
+                                </div>
+
+
                             </div>
                         </div>
                     </div>
